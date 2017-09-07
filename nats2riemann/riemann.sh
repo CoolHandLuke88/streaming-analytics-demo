73e226524d760e51d98c2daed7cf9d7ef40916ee
Dmitry Dorofeev <dima@yasp.com>
2017-09-07 18:25:06 +0300

    fixed sh script

--- nats2riemann/riemann.sh
+++ nats2riemann/riemann.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-EXTRA_CLASSPATH=/Users/deem0n/Documents/Projects/LuxmsBI/clj-nats-streaming/target/uberjar/clj-nats-streaming-0.1.0-SNAPSHOT-standalone.jar riemann start riemann.config
+riemann start riemann.config
