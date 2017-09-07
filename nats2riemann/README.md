f5071c9a1d857ef6bc9bf160ad36d39f917d69aa
Dmitry Dorofeev <dima@yasp.com>
2017-09-07 18:23:58 +0300

    nats2riemann

---
+++ nats2riemann/README.md
@@ -0,0 +1,19 @@
+# nats2riemann.js
+
+The sample script which subscribes to the NATS Streaming subject and sends all messages to the Riemann.
+
+## Installation
+
+```
+git clone https://github.com/luxms/streaming-analytics-demo.git
+cd nats2riemann
+npm install
+```
+
+## How to run
+
+```
+node nats2riemann.js
+```
+
+## License MIT
---
+++ nats2riemann/nats2riemann.js
@@ -0,0 +1,72 @@
+var natsNodeStreaming = require('node-nats-streaming');
+var riemann = require('riemann');
+var colors = require('colors');
+
+
+function createRiemannConnection() {
+  return new Promise((resolve, reject) => {
+    var riemannClient = riemann.createClient({
+      host: "localhost",
+      port: 5555
+    });
+
+    riemannClient.on('connect', () => {
+      console.log('Riemann - connected');
+      resolve(riemannClient);
+      // client.publish('presence', 'Hello mqtt');
+    });
+
+  });
+}
+
+
+function createNatsConnection() {
+  return new Promise((resolve, reject) => {
+    var srv = 'nats://192.168.103.7:4222';
+    srv = 'nats://localhost:4222'
+    var server = natsNodeStreaming.connect('test-cluster', 'thingenix-riemann-pusher', srv);
+    server.on('connect', function () {
+      console.log('NATS - connected');
+      resolve(server);
+    });
+  });
+}
+
+
+// main
+Promise.all([createRiemannConnection(), createNatsConnection()])
+.then(([riemannConnection, natsConnection]) => {
+  console.log('Connected to Riemann & NATS');
+
+  var opts = natsConnection.subscriptionOptions().setDeliverAllAvailable();
+  var natsSubscription = natsConnection.subscribe('thingenix', opts);
+
+  var i = 0;
+  natsSubscription.on('message', (msg) => {
+    // message is Buffer
+    if (i++ % 50000 == 0) {
+      console.log("Got from Nats".underline.blue, i, "messages".underline.blue);
+      console.log(msg.getData());
+    }
+
+    var message = JSON.parse(msg.getData());
+    for (a in message["items"]) {
+      if ( message["timestamp"] === undefined ) {
+         continue;
+      }
+      var d = new Date(message["timestamp"]);
+      riemannConnection.send(riemannConnection.Event({
+          host: message["_id"],
+          service: a,
+          time: d.getTime() / 1000,
+          metric: message["items"][a],
+          attributes: [{key:"longitude", value: message["loc"][0]}, {key:"latitude", value: message["loc"][1]},{key:"context", value: message["context"]}], //custom fields
+          //tags: ['nonblocking']
+          ttl: 100000
+        }), riemannConnection.tcp);
+    }
+  });
+})
+.catch((err) => {
+  console.log(err);
+});
